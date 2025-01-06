365c2cd110c5b1ce52a7fce0fcc2417b589657f0
Gaurav Kumar Rana <rana.gaurav17@gmail.com>
2025-01-06 20:12:57 +0530

    Adds test case to handle new line with comma seprated string in Add function

--- src/index.test.js
+++ src/index.test.js
@@ -7,4 +7,7 @@ describe("Test suite", () => {
   it("should return 3 if input is '1, 2'", () => {
     expect(Add("1, 2")).toBe(3); // comma separated number should return the sum of numbers in the string
   });
+  it("should handle new line and comma in the input '1\n2,3'", () => {
+    expect(Add("1\n2,3")).toBe(6); // new line and comma separated numbers should return the sum of numbers in the string
+  });
 });
