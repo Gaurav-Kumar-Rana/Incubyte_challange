88aec41c5d7caedf8ae209e4d6f74067b9aed309
Gaurav Kumar Rana <rana.gaurav17@gmail.com>
2025-01-06 20:08:14 +0530

    Adds logic to add numbers in string seprated by delimiter(",")

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
-const Add = (str)=>{
-    if (str === "") return 0;
-}
+const Add = (str) => {
+  if (str === "") return 0;
+  const numbers = str.split(",").map(Number);
+  return numbers.reduce((a, b) => a + b, 0);
+};
 
 module.exports = Add;
