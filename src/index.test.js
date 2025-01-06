a99cad5354fb065463947fcd5d04878c14cd5706
Gaurav Kumar Rana <rana.gaurav17@gmail.com>
2025-01-06 20:04:14 +0530

    Add test case to sum numbers if they are seprated by any delimiter in this case it is comma(",")

--- src/index.test.js
+++ src/index.test.js
@@ -4,4 +4,7 @@ describe("Test suite", () => {
   it("should return 0 ", () => {
     expect(Add("")).toBe(0); // empty string should return 0
   });
+  it("should return 3 if input is '1, 2'", () => {
+    expect(Add("1, 2")).toBe(3); // comma separated number should return the sum of numbers in the string
+  });
 });
